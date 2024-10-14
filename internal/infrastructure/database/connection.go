4cba8df7b8fcc9e7aa1eb873071785c8c674e328
Daniyal Zhanturin <daniyal5536@gmail.com>
2024-10-14 16:11:43 +0500

    feature: reduce ping timeout

--- internal/infrastructure/database/connection.go
+++ internal/infrastructure/database/connection.go
@@ -19,7 +19,7 @@ func NewDB(databaseDSN string) *sql.DB {
 		log.Fatalf("Failed to open database: %v", err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	err = db.PingContext(ctx)
 	if err != nil {
